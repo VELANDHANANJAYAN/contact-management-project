4a065306b5cf43e4ae15725499010231bda1ee35
Velan <119405038+VELANDHANANJAYAN@users.noreply.github.com>
2025-01-22 23:30:02 +0530

    Create contact.js

---
+++ server/contact.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  location: { type: String, required: true },
+  status: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
